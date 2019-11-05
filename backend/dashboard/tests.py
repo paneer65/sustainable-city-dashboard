34edaace661229e77858f1a4c1d6d3b0b3bde980
Rohan Anand <thededlier@gmail.com>
2019-11-05 11:43:39 +0000

    sanity test

--- backend/dashboard/tests.py
+++ backend/dashboard/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SanityTest(TestCase):
+    def test_addition_sanity(self):
+        print('Method: test_addition_sanity')
+        self.assertEqual(1 + 1, 2)
