75161607728c53f66cf40d8d61613822b29c074f
esmonddsouza <56561255+esmonddsouza@users.noreply.github.com>
2019-11-18 17:16:30 +0000

    Maps Dashboard (#11)
    * Maps API Built.
    
    * cleanup

--- frontend/src/routes/index.js
+++ frontend/src/routes/index.js
@@ -3,11 +3,13 @@ import { Switch } from "react-router-dom";
 import Route from "./Route";
 
 import Login from "../components/login";
+import Dashboard from "../components/dashboard";
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/login" exact component={Login} />
+      <Route path="/dashboard" exact component={Dashboard} />
 
       {/* redirect user to Login page if route does not exist and user is not authenticated */}
       <Route component={Login} />
