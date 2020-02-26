da0f0414cfe512b790a76f784c2540f185481cb7
esmonddsouza <56561255+esmonddsouza@users.noreply.github.com>
2020-02-26 10:38:40 +0000

    normalised pollutant values (#48)

--- frontend/src/components/map/Map.js
+++ frontend/src/components/map/Map.js
@@ -12,105 +12,100 @@ const defaultLocation = { lat: 53.343786, lng: -6.255828 };
 const defaultZoomLevel = 11;
 const googleMapURL = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyDOjyfAl22KFpq0czq_I0sbRtJHKEkwdIc";
 const pollutionURL = "https://api.openaq.org/v1/latest?coordinates=53.34399,-6.26719&radius=10000&order_by=distance";
+const ozoneWeight = 0.3;
+const sulphurDioxideWeight = 0.15;
+const nitrogenDioxideWeight = 0.10;
+const particleMatter25Weight = 0.20;
+const particleMatter10Weight = 0.20;
+const carbonMonoxideWright = 0.5;
 
 class Map extends React.Component {
 	constructor(props) {
     super(props)
     this.state = {
-      pollutionData: [],
-      selectedMarker: false,
-			selectedFilter: '',
+      	pollutionData: [],
+      	selectedMarker: false,
+		selectedFilter: '',
     }
   }
 
-  componentDidUpdate(prevProps) {
+	componentDidUpdate(prevProps) {
 		// Need to check state selectedFilter otherwise this will be an infinite loop
 		if (this.props.selectedFilter === 'Pollution' && prevProps.selectedFilter !== 'Pollution') {
-	    	this.fetchPollutionMapData();
+			this.fetchPollutionMapData();
 		}
-  }
+	}
 
-  	airQualityCalculator(data) {
-		for ( var key in data.results ) {
-			var pollutionLevel = 'Low';
+	normalizeMapValues(mapTobeNormalized, listOfValues) { 
+		let pollutantMap = new Map(mapTobeNormalized);
+		let mapValues = Object.values(pollutantMap.props);
+		let maxValue = Math.max.apply(null, listOfValues);
+		let minValue = Math.min.apply(null, listOfValues);
+		let normalisedMap = new Map();
+		mapValues.map((value, key) => {
+			if(typeof value === "number"){
+				let normalizedValue = (value - minValue)/ (maxValue - minValue);
+				normalisedMap[key] = normalizedValue;
+			}
+		});
+		return normalisedMap;
+	}
+
+
+	airQualityCalculator(data) {
+		let pollutionLevelMap = new Map();
+		let pollutionLevelArray = new Array();
+		let keys = Object.keys(data.results);
+		for ( var key in keys ) {
+			var pollutionLevel = 0;
 			var hazardousPollutant = "";
 			for ( var pollutantData in data.results[key]["measurements"] ) {
 				var pollutantValue = data.results[key]["measurements"][pollutantData]["value"]
 				var parameter = data.results[key]["measurements"][pollutantData]["parameter"]
 				switch ( parameter ) {
-
 					case "o3":
-						if ( pollutantValue >= 101 && pollutantValue <= 187 ) {
-							if ( pollutionLevel !== 'High')
-								pollutionLevel = 'Medium';
-						}
-						else if ( pollutantValue >= 188 ) {
-							pollutionLevel = 'High';
-							hazardousPollutant = parameter ;
-						}
-						else
-							continue;
-          break
+						pollutionLevel = pollutionLevel + ozoneWeight*pollutantValue;
+						break
 
 					case "so2":
-						if ( pollutantValue >= 267 && pollutantValue <= 710 ) {
-							if ( pollutionLevel !== 'High')
-								pollutionLevel = 'Medium';
-						}
-						else if ( pollutantValue >= 711 ) {
-							pollutionLevel = 'High';
-							hazardousPollutant = parameter ;
-						}
-						else
-							continue;
-          break
+						pollutionLevel = pollutionLevel + sulphurDioxideWeight*pollutantValue;
+						break
 
 					case "no2":
-						if ( pollutantValue >= 201 && pollutantValue <= 467 ) {
-							if ( pollutionLevel !== 'High')
-								pollutionLevel = 'Medium';
-						}
-						else if ( pollutantValue >= 468 ) {
-							pollutionLevel = 'High';
-							hazardousPollutant = parameter ;
-						}
-						else
-							continue;
-          break
+						pollutionLevel = pollutionLevel + nitrogenDioxideWeight*pollutantValue;
+						break
 
 					case "pm25":
-						if ( pollutantValue >= 36 && pollutantValue <= 58 ) {
-							if ( pollutionLevel !== 'High')
-								pollutionLevel = 'Medium';
-						}
-						else if ( pollutantValue >= 59 ) {
-							pollutionLevel = 'High';
-							hazardousPollutant = parameter ;
-						}
-						else
-							continue;
-          break
+						pollutionLevel = pollutionLevel + particleMatter25Weight*pollutantValue;
+						break
 
 					case "pm10":
-						if ( pollutantValue >= 51 && pollutantValue <= 83 ) {
-							if ( pollutionLevel !== 'High')
-								pollutionLevel = 'Medium';
-						}
-						else if ( pollutantValue >= 84 ) {
-							pollutionLevel = 'High';
-							hazardousPollutant = parameter ;
-						}
-						else
-							continue;
-          break
+						pollutionLevel = pollutionLevel + particleMatter10Weight*pollutantValue;
+						break
 
-					default: continue;
+					case "co":
+						pollutionLevel = pollutionLevel + carbonMonoxideWright*pollutantValue;
+						break;
+
+					default: 
+						continue;
 				}
 			}
-			data.results[key]["pollutionLevel"] = pollutionLevel
-			data.results[key]["hazardousPollutant"] = hazardousPollutant
-			data.results[key]["markerKey"] = key
+			pollutionLevelMap[key] = pollutionLevel;
+			pollutionLevelArray.push(pollutionLevel);
+			data.results[key]["hazardousPollutant"] = hazardousPollutant;
+			data.results[key]["markerKey"] = key;
+			//data.results[key]["pollutionLevel"] = 0;
 		}
+		let normalizedPollutantLevelMap = new Map(this.normalizeMapValues(pollutionLevelMap, pollutionLevelArray));
+		for (const [key, value] of Object.entries(normalizedPollutantLevelMap.props)) {
+			if(typeof value === "number"){
+				console.log(key + " asdasd " +value)
+				console.log(data.results[key])
+				data.results[key]["pollutionLevel"] = value;
+			}
+		}
+		debugger;
 		return data
 	}
 
@@ -120,12 +115,12 @@ class Map extends React.Component {
 			.then(data => {
 				const updatedData = this.airQualityCalculator(data)
 				this.setState({ pollutionData: updatedData.results })
-			})
+		})
 	}
 
-  handleClick(marker, event) {
-    this.setState({ selectedMarker: marker });
-  }
+	handleClick(marker, event) {
+		this.setState({ selectedMarker: marker });
+	}
 
 	googleMapInit() {
 		return (
@@ -135,56 +130,52 @@ class Map extends React.Component {
 
 	generatePollutionMap() {
 		return compose(withScriptjs, withGoogleMap)(props => {
-		  return (
-		    <GoogleMap defaultZoom={ defaultZoomLevel } defaultCenter={ defaultLocation }>
-		      {
+		  	return (
+				<GoogleMap defaultZoom={ defaultZoomLevel } defaultCenter={ defaultLocation }>
+				{
 						props.markers.map(marker => {
-			        const onClick = props.onClick.bind(this, marker)
-			        return (
-			          <Marker
-			            key={marker.location}
-			            onClick={onClick}
-			            position={{ lat: marker.coordinates.latitude, lng: marker.coordinates.longitude }}
-								>
-			          	{
-										props.selectedMarker === marker &&
-			              <InfoWindow>
-			              	<div>
-			              		<h4>{marker.location}</h4>
-								{marker.pollutionLevel} <br/>
-							  	{marker.hazardousPollutant}
-			                </div>
-			              </InfoWindow>
-									}
-			          </Marker>
-			        )
-		      	})
-					}
-		    </GoogleMap>
-		  )
+							const onClick = props.onClick.bind(this, marker)
+							return (
+								<Marker	key={marker.location}
+										onClick={onClick}
+										position={{ lat: marker.coordinates.latitude, lng: marker.coordinates.longitude }}>
+								{
+									props.selectedMarker === marker &&
+									<InfoWindow>
+										<div>
+											<h4>{marker.location}</h4>
+											{marker.pollutionLevel} <br/>
+											{marker.hazardousPollutant}
+										</div>
+									</InfoWindow>
+								}
+								</Marker>
+							)
+					})
+				}
+				</GoogleMap>
+		  	)
 		});
 	}
 
 	render() {
 		if (this.props.selectedFilter === 'Pollution') {
 			const PollutionMap = this.generatePollutionMap();
-
 			return (
 				<PollutionMap
-	        selectedMarker={ this.state.selectedMarker }
-	        markers={ this.state.pollutionData }
-	        onClick={ this.handleClick }
-	        googleMapURL={ googleMapURL }
-	        loadingElement={<div style= {{ height: `100%` }} />}
-	        containerElement={<div style= {{ height: `100%` }} />}
-	        mapElement={<div style = {{ height: `100%` }} />}
-	      />
+				selectedMarker={ this.state.selectedMarker }
+				markers={ this.state.pollutionData }
+				onClick={ this.handleClick }
+				googleMapURL={ googleMapURL }
+				loadingElement={<div style= {{ height: `100%` }} />}
+				containerElement={<div style= {{ height: `100%` }} />}
+				mapElement={<div style = {{ height: `100%` }} />}
+	      		/>
 			)
 		}
 
 		else {
 			const WrappedMap = withScriptjs(withGoogleMap(this.googleMapInit))
-
 			return (
 				<WrappedMap
 					googleMapURL = { googleMapURL }
